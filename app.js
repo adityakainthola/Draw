aa0d6c94440e90566d0ff9641fa6e154fbeec681
unknown <adityakainthola5306365@gmail.com>
2022-08-05 21:14:22 +0530

    helo

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express"); // access
+const socket = require("socket.io");
+
+const app = express(); // initialise the application  and sever ready
+app.use(express.static("public"));
+let port = 5000;
+
+let server = app.listen(port, () => {
+  console.log("listening to port " + port);
+});
+
+let io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log("Made socket connection");
+    // Received data
+    socket.on("beginPath", (data) => {
+        // data -> data from frontend
+        // Now transfer data to all connected computers
+        io.sockets.emit("beginPath", data);
+    })
+    socket.on("drawStroke", (data) => {
+        io.sockets.emit("drawStroke", data);
+    })
+    socket.on("redoUndo", (data) => {
+        io.sockets.emit("redoUndo", data);
+    })
+})
+
+
